fb0345c89515951b8de9af48407ae39edecb16f0
zsl <zhoushunli12@gmail.com>
2018-02-03 16:28:17 +0800

    提交

---
+++ app/src/main/java/com/example/zsl/myapplication/ActivityTest.java
@@ -0,0 +1,46 @@
+package com.example.zsl.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ActivityTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ActivityTest.this, "you click button1", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/zsl/myapplication/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.example.zsl.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by zsl on 2018/1/20.
+ */
+
+public class SecondActivity extends Activity {
+}
